3945d94e416c083ba1ff22984a39cda5a9f11de9
Victoria <victoria.sofianidou@gmail.com>
2020-12-07 17:01:21 +0100

    finished step 9

--- project-name/src/app/app.component.ts
+++ project-name/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'project-name';
+  /*selected;*/
+  languages = [
+    {
+      name: 'Javascript'
+    },
+    {
+      name: 'PHP'
+    },
+
+    {
+      name: 'Python'
+    }
+  ];
 }
+
